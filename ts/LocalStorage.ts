e363b8e7c48a5a45071711c092e272966960e886
yango3 <o3.yang@hotmail.com>
2018-03-22 11:46:52 +0800

    change version num to 1.2.6

--- ts/LocalStorage.ts
+++ ts/LocalStorage.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import { AppConfig } from "./AppConfig";
 import { Tool } from "./utility";
 
-const APP_VERSION = "1.2.5";
+const APP_VERSION = "1.2.6";
 // require("../package.json").version;
 
 const UI_VERSION = "1.1";
